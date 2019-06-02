50450e0c85ee57e6975d32f8551a016f04eca0d7
tishparichkova <tihomira.parichkova@fourth.com>
2019-06-02 10:13:42 +0300

    Updates

---
+++ precache-manifest.c6f5acb0af4742224cec593df8afee7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3664d4f8f4bb06da20e6f8169d44565",
+    "url": "/tic-tac-toe-university/index.html"
+  },
+  {
+    "revision": "6483cdb3b5aa137ad1db",
+    "url": "/tic-tac-toe-university/static/css/main.9f0f301d.chunk.css"
+  },
+  {
+    "revision": "f1a9a1336605f00956cb",
+    "url": "/tic-tac-toe-university/static/js/2.128dbfaf.chunk.js"
+  },
+  {
+    "revision": "6483cdb3b5aa137ad1db",
+    "url": "/tic-tac-toe-university/static/js/main.401505a7.chunk.js"
+  },
+  {
+    "revision": "285aa26779955d2c8e3e",
+    "url": "/tic-tac-toe-university/static/js/runtime~main.3d0366a4.js"
+  }
+]);
